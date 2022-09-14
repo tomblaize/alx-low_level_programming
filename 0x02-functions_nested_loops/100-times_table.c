0fe33579b29ce931deedd5eb102f4097c155d8da
Tomblaize <thomasegbah.com>
2022-09-14 10:41:08 -0700

    a function that prints the n times table, starting with 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,16 +4,16 @@
  * print_times_table - prints times table
  * @n : times table to use
  * Return:void
- */
+ **/
 void print_times_table(int n)
-{
+	{
 
-int a = 0, rep, b;
+	int a = 0, rep, b;
 
-if (n < 0 || n > 15)
+	if (n < 0 || n > 15)
 	return;
 while (a <= n)
-{
+	{
 	for (b = 0; b <= n; b++)
 	{
 		rep = a * b;
