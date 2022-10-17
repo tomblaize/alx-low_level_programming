a7835dc79f0a53a99ee2f7173ad4f37347c3aa54
Tomblaize <thomasegbah.com>
2022-10-17 11:55:23 -0700

    a new type struct dog with the following elements

---
+++ dog.h
@@ -0,0 +1,25 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - is a structure for a dog object
+ * @name: is the name element
+ * @age: is the age element
+ * @owner: is the owner element
+ */
+
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+typedef struct dog dog_t;
+void print_dog(struct dog *d);
+void free_dog(dog_t *d);
+dog_t *new_dog(char *name, float age, char *owner);
+#endif /*DOG_H*/
